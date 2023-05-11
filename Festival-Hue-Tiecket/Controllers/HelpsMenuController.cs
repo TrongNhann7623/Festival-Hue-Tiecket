6edccaf5c1408d425b124a63ff09001b4bcb2736
Nhân Phan <trongnhan7623@gmail.com>
2023-05-11 16:01:59 +0700

    Add project files.

---
+++ Festival-Hue-Tiecket/Controllers/HelpsMenuController.cs
@@ -0,0 +1,24 @@
+﻿using Festival_Hue_Tiecket.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Festival_Hue_Tiecket.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelpsMenuController : ControllerBase
+    {
+        public static List<HelpsMenu> helpsMenus = new List<HelpsMenu>();
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(helpsMenus);
+        }
+        [HttpPost]
+        public IActionResult Create
+    }
+}
---
+++ Festival-Hue-Tiecket/Models/LocationLiked.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Festival_Hue_Tiecket.Models
+{
+    public class LocationLiked
+    {
+
+    }
+}
